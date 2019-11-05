3187c815b216d2c05c55d475b329fe65ff222ab8
Andres Colonia <andreskal@gmail.com>
2019-11-05 19:39:37 -0300

    feat: include goroutine with sync group and flags to send parameters

--- main.go
+++ main.go
@@ -1,19 +1,32 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net"
-	"strconv"
+	"sync"
 )
 
-func main() {
-	for port := 1; port <= 10000; port++ {
-		_, err := net.Dial("tcp", "localhost:"+strconv.Itoa(port))
-		if err == nil {
-			fmt.Println("Port " + strconv.Itoa(port) + " is open")
-		} else {
-			fmt.Println(err)
-		}
+var wg = &sync.WaitGroup{}
 
+func main() {
+	hostname := flag.String("hostname", "localhost", "hostname to scan ports")
+	startPort := flag.Int("start-port", 1, "the start port to scan")
+	endPort := flag.Int("end-port", 100, "the end port to scan")
+
+	for i := *startPort; i <= *endPort; i++ {
+		wg.Add(1)
+		go Scanner(*hostname, i)
 	}
+	wg.Wait()
+}
+
+func Scanner(url string, port int) {
+	_, err := net.Dial("tcp", fmt.Sprintf("%s:%d", url, port))
+	if err == nil {
+		fmt.Printf("%s port %d is open\n", url, port)
+	} else {
+		fmt.Println(err)
+	}
+	defer wg.Done()
 }
